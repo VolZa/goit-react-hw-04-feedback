83125a0b02702e654e5b0e4fe355fae645f5f45a
VolZa <vvolza@gmail.com>
2023-05-04 15:42:44 +0300

    Стилізація Статистики

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StatisticS = styled.ul`
+  display: flex;        
+  flex-direction: column;
+  gap: 10px;
+  padding: 0px;
+  `;
+  export const Statistic = styled.li`
+    display:flex;
+    `
